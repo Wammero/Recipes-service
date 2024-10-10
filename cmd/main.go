0950cb12d12ba91f5d88e6783b216a9d4cb3b4f0
Wammero <107371364+Wammero@users.noreply.github.com>
2024-10-10 23:50:21 +0300

    update and add no docker migrations (local)

--- cmd/main.go
+++ cmd/main.go
@@ -3,12 +3,39 @@ package main
 import (
 	"log"
 	"recipe/pkg/api"
+	"recipe/repository"
+
+	"github.com/golang-migrate/migrate/v4"
+	_ "github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
 
 	"github.com/gorilla/mux"
 )
 
 func main() {
-	api := api.New(mux.NewRouter())
+	db, err := repository.New("postgresql://root:1234@0.0.0.0:5441/bd_1?sslmode=disable")
+	if err != nil {
+		log.Fatal("Failed to connect to the database:", err)
+	}
+
+	migration1, err := migrate.New(
+		"file://C:/Users/nazhmud1/Desktop/projects/Recipes-service/migrations",
+		"postgresql://root:1234@0.0.0.0:5441/bd_1?sslmode=disable",
+	)
+	if err != nil {
+		log.Fatalln("Cannot start migration:", err)
+	}
+
+	if err := migration1.Up(); err != nil {
+		if err == migrate.ErrNoChange {
+			log.Println("No migrations to apply, continuing...")
+		} else {
+			log.Fatalln("Migration failed:", err)
+		}
+	}
+
+	api := api.New(mux.NewRouter(), db)
 	api.Handle()
+
 	log.Fatal(api.ListenAndServe("localhost:8080"))
 }
