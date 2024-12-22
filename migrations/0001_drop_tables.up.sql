7a7754378ccdb7d975404e1a6709eeffc63857be
Wammero <107371364+Wammero@users.noreply.github.com>
2024-12-22 20:33:44 +0300

    architecture update
    The architecture has been updated

--- migrations/0001_drop_tables.up.sql
+++
@@ -1,7 +0,0 @@
-DROP TABLE IF EXISTS recipe_ingredient_links;
-
-DROP TABLE IF EXISTS recipes;
-
-DROP TABLE IF EXISTS recipe_ingredients;
-
-DROP TABLE IF EXISTS users;
--- pkg/api/change-favourite.go
+++
@@ -1,9 +0,0 @@
-package api
-
-import (
-	"net/http"
-)
-
-func (api *api) ChangeFavoutite(w http.ResponseWriter, r *http.Request) {
-
-}
